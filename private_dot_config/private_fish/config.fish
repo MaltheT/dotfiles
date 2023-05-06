64b213259824b7d6ec6dc186e8ebdeb6e68a9723
MaltheT <malthe@toettrup.dk>
2023-05-06 12:09:30 +0200

    fish config

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    	# Commands to run in interactive sessions can go here
+    	alias sfox='bass source /opt/ros/foxy/setup.bash'
+	alias sins='bass source install/setup.bash'
+end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/malthe/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
